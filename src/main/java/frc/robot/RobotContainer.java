42418ac9b256b134bc2871ee5731d8fc7b91c2a3
ThunderBladeC <connor.j.ip@gmail.com>
2025-01-17 14:59:40 -0500

    a + b binds foward backwards

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,14 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.Autos;
+import frc.robot.subsystems.ExampleSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -25,8 +22,10 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public final static CommandXboxController m_CommandXboxController = new CommandXboxController(0);
-  public static WPI_TalonSRX motor1 = new WPI_TalonSRX(0);
-  public static WPI_TalonSRX motor2 = new WPI_TalonSRX(1);
+  public static WPI_TalonSRX motor1 = new WPI_TalonSRX(1);
+  public static WPI_TalonSRX motor2 = new WPI_TalonSRX(0);
+  public static WPI_TalonSRX motor3 = new WPI_TalonSRX(2);
+  public static WPI_TalonSRX motor4 = new WPI_TalonSRX(3);
   public static ExampleSubsystem mExampleSubsystem = new ExampleSubsystem();
 
   DifferentialDrive diffDrive; 
@@ -37,7 +36,16 @@ public class RobotContainer {
     // motor2.set(0.2);
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_CommandXboxController.a().whileTrue(
+      Commands.run(() -> motor1.set(0.2))
+      .finallyDo(() -> motor1.set(0))
+    );
+    m_CommandXboxController.b().whileTrue(
+      Commands.run(() -> motor1.set(-0.2))
+      .finallyDo(() -> motor1.set(0))
+    );
+  }
 
   public Command getAutonomousCommand() {
     return Autos.exampleAuto(mExampleSubsystem);
